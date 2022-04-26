ddd62b3b26619df3c340df6b4549841363955569
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-04-26 22:19:44 +0530

    feat:route for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Home} from './pages'
-const App:React.FC= ()=> {
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages";
+
+const App: React.FC = () => {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
